b2e17ca34bb1b37cee2b0fc133bc5c8bf4772713
Heath-Pat <ovouncleheath9850@yahoo.com>
2025-08-06 19:33:06 -0400

    added a Player(CircleShape) class that has triangle method and a draw method

--- main.py
+++ main.py
@@ -2,21 +2,25 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-from constants import * 
+from constants import *
+from player import * 
 def main():
-
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    pygame.init()
+    player1 = Player((SCREEN_WIDTH / 2),(SCREEN_HEIGHT / 2))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
     
     while True:
+        screen.fill('black')
+        player1.draw(screen)
+        pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 100
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
-
-        screen.fill('black')
-        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
