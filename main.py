38e63764acd266128a264f834f2db5d18019ce8c
Heath-Pat <ovouncleheath9850@yahoo.com>
2025-08-07 04:24:37 -0400

    added collision dection and asteroids into the game

--- main.py
+++ main.py
@@ -2,25 +2,49 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import sys
 from constants import *
-from player import * 
+from player import *
+from asteroid import *
+from asteroidfield import * 
 def main():
     
     pygame.init()
-    player1 = Player((SCREEN_WIDTH / 2),(SCREEN_HEIGHT / 2))
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    all_asteroids = pygame.sprite.Group()
+     
+    Asteroid.containers = (all_asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+    player1 = Player((SCREEN_WIDTH / 2),(SCREEN_HEIGHT / 2))
+    
     delta_time = 0
     
     while True:
         
         screen.fill('black')
-        player1.draw(screen)
-        player1.update(delta_time)
+        clock.tick(50)
+
+        for obj in drawable:
+            obj.draw(screen)
+        
+        updatable.update(delta_time)
+        for obj in all_asteroids:
+            if obj.check_collision(player1):
+                print('Game over!')
+                sys.exit('Exiting')
+        
         pygame.display.flip()
-        clock.tick(60)
+        
         
         delta_time = clock.tick(60) / 100
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
