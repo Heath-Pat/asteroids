35c3cac1b379c44b7f7a3bb9ef2f830d73392950
Heath-Pat <ovouncleheath9850@yahoo.com>
2025-08-06 20:23:56 -0400

    made so we can rotate the triangle left and right

--- main.py
+++ main.py
@@ -10,13 +10,17 @@ def main():
     player1 = Player((SCREEN_WIDTH / 2),(SCREEN_HEIGHT / 2))
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    delta_time = 0
     
     while True:
+        
         screen.fill('black')
         player1.draw(screen)
+        player1.update(delta_time)
         pygame.display.flip()
         clock.tick(60)
-        dt = clock.tick(60) / 100
+        
+        delta_time = clock.tick(60) / 100
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
